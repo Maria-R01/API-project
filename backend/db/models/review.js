040dc26fde6e04d7950f5f024fabc7e9169d301d
Maria <femaleintech23@gmail.com>
2023-07-10 18:24:57 -0700

    Create models and migrations

---
+++ backend/db/models/review.js
@@ -0,0 +1,40 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Review.init({
+    spotId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Spot'
+      }
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User'
+      }
+    },
+    review: {
+      type: DataTypes.STRING,
+    },
+    stars: {
+      type: DataTypes.INTEGER
+    },
+  }, {
+    sequelize,
+    modelName: 'Review',
+  });
+  return Review;
+};
---
+++ backend/db/models/spot.js
@@ -0,0 +1,72 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Spot.init({
+    ownerId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User'
+      }
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lat: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        
+      },
+    },
+    lng: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [4, 49]
+      }
+    },
+    description: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    modelName: 'Spot',
+  });
+  return Spot;
+};
