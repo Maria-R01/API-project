a8e749d4a075a1749b331abe0274999d4b742140
Maria <femaleintech23@gmail.com>
2023-07-12 03:18:06 -0700

    Working on endpoints

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,79 +3,71 @@ const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
-// const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
 const { User, Spot, sequelize, SpotImage, Review } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
-// //creates a new spot
-// router.post('/', async (req, res) => {
-//     const { address, city, state, country, lat, lng, name, description, price } = req.body;
-//     const owner = await User.findOne()
-//     const newSpot = await Spot.create({
-//     ownerId: owner.id,
-//     address,
-//     city, 
-//     state,
-//     country, 
-//     lat, 
-//     lng, 
-//     name, 
-//     description, 
-//     price  
-//     })
+//creates a new spot
+router.post('/', requireAuth, async (req, res) => {
+    const { address, city, state, country, lat, lng, name, description, price } = req.body;
+    const owner = await User.findOne()
+    const newSpot = await Spot.create({
+    ownerId: owner.id,
+    address,
+    city, 
+    state,
+    country, 
+    lat, 
+    lng, 
+    name, 
+    description, 
+    price  
+    })
 
-//     res.json(newSpot); 
-// })
+    res.json(newSpot); 
+}, handleValidationErrors)
 
 //get all spots
 router.get('/', async (req, res) => {
-    // const image = {};
-    const spots = await Spot.findAll({
-        include:{
-            model: Review,
-            attributes: [],
-          },
-          attributes: {
-            include: [
-                // ['url', 'previewImage'],
-              [
-                sequelize.fn('AVG', sequelize.col('stars')), 'avgRating'
-              ], 
-            ],
-          },
-    //     attributes: [['Review.url', 'previewImage'],  
-    //         [sequelize.fn('AVG', sequelize.col('stars')), 'avgRating'
-    //       ],
-    //     ]
-    // });
-    // const avgRating = await Review.findAll({
-    //     include: {
-    //         attributes: ['stars'],
-    //     },
-    //     attributes: {
-    //         include: [[sequelize.fn('AVG', sequelize.col('stars')), 'avgRating']],
-    //     }
+    const avgRating = await Review.findAll({
+      attributes: [[sequelize.fn('AVG', sequelize.col('stars')), 'avgRating']],
+  });
+    const Spots = await Spot.findAll({
+      // attributes: {
+      //   include: [[sequelize.fn('AVG', sequelize.col('stars')), 'avgRating']],
+      // },
+      //   attributes: {
+      //     include: [
+      //       [
+      //         sequelize.fn('AVG', sequelize.col('stars')), 'avgRating'
+      //       ],
+      //     ]
+      // },
+        //   // attributes: {
+        //   //   include: [
+        //   //       // ['url', 'previewImage'],
+        //   //     [
+        //   //       sequelize.fn('AVG', sequelize.col('stars')), 'avgRating'
+        //   //     ], 
+        //   //   ],
+        //   // },
+        // attributes: [  
+        //     [sequelize.fn('AVG', sequelize.col('stars')), 'avgRating'],
+        // ]
     });
 
-    // image.previewImage = await SpotImage.findOne({
-    //     include: {
-    //         model: spots,
-    //         where: {
-    //             spotId: {
-    //                 [Op.eq]: spots.id
-    //             }
-    //     },
-    //     },
-    //     attributes: [['url', 'previewImage']]
-    // })
-    // console.log(image)
-    console.log(spots);
-    res.json(spots);
+    // for(let i = 0; i < Spots.length; i++){
+    //   const obj = Spots[i].toJSON();
+
+    // }
+
+    res.json({Spots});
+    // res.json(avgRating)
 });
   
 
 
-module.exports =  router;
+module.exports = router;
