2b7855f4a2b5433c287fdc74e35d1123b82a3a06
Maria <femaleintech23@gmail.com>
2023-07-28 17:34:37 -0700

    Added Navigation Componenet

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,9 @@
+const Bed = () => {
+    return (
+      <div style={{ color: "pink", fontSize: "100px" }}>
+        <i class="fa-solid fa-bed"></i>
+      </div>
+    );
+  };
+
+  export default Bed;
