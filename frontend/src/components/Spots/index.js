76b19e9c17c13a7d1e348a745ae0746aa7cc602e
Maria <femaleintech23@gmail.com>
2023-08-28 14:50:53 -0700

    some css added to landing page

--- frontend/src/components/Spots/index.js
+++ frontend/src/components/Spots/index.js
@@ -1,8 +1,9 @@
 import './Spots.css';
 import  { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { actionLoadSpots, loadSpotsThunk } from '../../store/spots';
-import { NavLink } from 'react-router-dom';
+import { loadSpotsThunk } from '../../store/spots';
+// import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom/cjs/react-router-dom';
 
 const Spots = () => {
     const allSpots = [];
@@ -12,12 +13,13 @@ const Spots = () => {
     }, [dispatch]);
     const spotsData = useSelector(state => state.spots);
     const spotsDataArr = Object.values(spotsData.allSpots);
+    console.log(spotsDataArr)
     spotsDataArr.map(spot => allSpots.push(spot));
     // console.log('SpotsData in spots component after useSelector: ', spotsData);
     // console.log('allSpots array: ', allSpots)
     return (
         <>
-        <h1>HELLO FROM SPOTS COMPONENT</h1>
+        <h1>All Spots Component</h1>
         <div className='allSpots'>
             {allSpots.map(spot => (
                 <div className='tile'>
@@ -28,16 +30,14 @@ const Spots = () => {
                             </div>
                             <div className='spotTileInfo'>
                                 <div>
-                                    <div className='spotAddress'>
-                                        {spot.city}, {spot.state}, {spot.country}
+                                    <div className='spotLocation'>
+                                        {spot.city}, {spot.state}
+                                        <div className='avgRating'>
+                                            <i className="fa-solid fa-star"></i> {spot.avgRating? spot.avgRating : `New`}
+                                        </div>
                                     </div>
                                     <div className='spotPrice'>
                                         ${spot.price}/night
-                                    </div>
-                                    <div className='avgRating'>
-                                    <i className="fa-solid fa-star">
-                                        {spot.avgRating}
-                                    </i>
                                     </div>
                                 </div>
                             </div>
