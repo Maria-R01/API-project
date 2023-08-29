250bf0035cd792014bb24bdc5b18b13f7166a114
Maria <femaleintech23@gmail.com>
2023-08-28 23:01:53 -0700

    moved code around

--- frontend/src/components/Spots/index.js
+++ frontend/src/components/Spots/index.js
@@ -2,21 +2,20 @@ import './Spots.css';
 import  { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { loadSpotsThunk } from '../../store/spots';
-// import { NavLink } from 'react-router-dom';
 import { NavLink } from 'react-router-dom/cjs/react-router-dom';
 
 const Spots = () => {
     const allSpots = [];
     const dispatch = useDispatch();
+    const spotsData = useSelector(state => state.spots);
+    const spotsDataArr = Object.values(spotsData.allSpots);
+    spotsDataArr.map(spot => allSpots.push(spot));
+
+
     useEffect(() => {
         dispatch(loadSpotsThunk());
     }, [dispatch]);
-    const spotsData = useSelector(state => state.spots);
-    const spotsDataArr = Object.values(spotsData.allSpots);
-    // console.log(spotsDataArr)
-    spotsDataArr.map(spot => allSpots.push(spot));
-    // console.log('SpotsData in spots component after useSelector: ', spotsData);
-    // console.log('allSpots array: ', allSpots)
+
     return (
         <>
         <h1>All Spots Component</h1>
