4ad32754d142c9cc76127945e813ee557f671192
Maria <femaleintech23@gmail.com>
2023-09-04 11:46:07 -0700

    css styling complete

--- frontend/src/components/DeleteReview/index.js
+++ frontend/src/components/DeleteReview/index.js
@@ -2,6 +2,7 @@ import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { deleteReviewThunk } from "../../store/reviews";
 import { loadSpecificSpotThunk } from "../../store/spots";
+import "./DeleteReview.css";
 
 const DeleteReview = ({ reviewId, spotId }) => {
     const { closeModal } = useModal();
@@ -16,16 +17,16 @@ const DeleteReview = ({ reviewId, spotId }) => {
 
 
     return (
-        <div>
-            <div>
-                <h1>Confirm Delete</h1>
+        <div className="deleteReviewContainer">
+            <div className="ConfirmDeleteHeading">
+                <h2>Confirm Delete</h2>
             </div>
-            <div>
+            <div className="deleteReviewQuestionContainer">
                 <div>
                     Are you sure you want to delete this review? 
                 </div>
             </div>
-            <div>
+            <div className="deleteReviewButtonsContainer">
                 <button className="yes" onClick={handleDelete}>Yes (Delete Review)</button>
                 <button className="no" onClick={closeModal}>No (Keep Review)</button>
             </div>
